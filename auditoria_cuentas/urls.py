4b9c13747670012f72f4263de3c5647d8078d14a
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2025-06-30 16:48:10 -0500

    jj

--- auditoria_cuentas/urls.py
+++ auditoria_cuentas/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import error_403
+from .views import error_403, landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('auditoria/', include('auditoria.urls')),
     path('cartera/', include('cartera.urls')),
     path('403/', error_403, name='403'),
+    path('', landing_page, name='landing'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler403 = 'auditoria_cuentas.views.error_403'
--- auditoria_cuentas/views.py
+++ auditoria_cuentas/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def error_403(request, exception=None):
-    return render(request, '403.html', status=403) 
+    return render(request, '403.html', status=403)
+
+def landing_page(request):
+    return render(request, 'landing.html') 
